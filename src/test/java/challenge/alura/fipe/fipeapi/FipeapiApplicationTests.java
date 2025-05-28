932d12196e919bf1e9ff5ea4a595f4346b2af3e6
Joao Lima <jplima1853@gmail.com>
2025-05-28 09:51:29 -0300

    crCommit inicial: criação do projeto

---
+++ src/test/java/challenge/alura/fipe/fipeapi/FipeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.alura.fipe.fipeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
